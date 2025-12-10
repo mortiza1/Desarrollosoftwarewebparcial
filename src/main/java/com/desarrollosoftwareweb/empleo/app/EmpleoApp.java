6c280255e463f5134c9bf682580fb43677e1f2c0
Tu Nombre <tu.email@ejemplo.com>
2025-12-10 15:44:38 -0500

    Commit inicial del proyecto empleo-app

---
+++ src/main/java/com/desarrollosoftwareweb/empleo/app/EmpleoApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.desarrollosoftwareweb.empleo.app;
+
+/**
+ *
+ * @author MOA
+ */
+public class EmpleoApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
